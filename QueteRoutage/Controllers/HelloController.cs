2d300896d0cf64a1ac5b60f9d0147e0bd5efced8
siwar <siwarsaid8@gmail.com>
2024-05-26 23:28:26 +0200

    ajoutFichier

---
+++ QueteRoutage/Controllers/HelloController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QueteRoutage.Controllers
+{
+    public class HelloController : Controller
+    {
+        public string Say()
+        {
+            return "Hello !";
+        }
+
+        public string Yell()
+        {
+            return "HELLO !";
+        }
+
+        [Route("/{controller}/Say")]
+        public string SayAttribut() 
+        {
+            return Yell();
+        }
+
+
+        [Route("/controller}/YELL")]
+        public string YellAttribut()
+        {
+            return Yell();
+        }
+    }
+}
